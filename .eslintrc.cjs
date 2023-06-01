ac6cbaeea5e106cd05c9c607350c747ed6d3ffb3
Kutimo <marius-oliver@hotmail.com>
2023-06-01 14:47:17 +0200

    added ts eslint plugins.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,16 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
   plugins: ["react-refresh", "prettier"],
   rules: {
     "react-refresh/only-export-components": "warn",
