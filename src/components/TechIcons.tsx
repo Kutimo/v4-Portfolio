5c841c3436996efa951345b337de705c082dc4ce
Kutimo <marius-oliver@hotmail.com>
2023-07-25 20:37:22 +0200

    Added gradients and logic for displaying the power bar under  tech icons.

--- src/components/TechIcons.tsx
+++ src/components/TechIcons.tsx
@@ -1,6 +1,24 @@
 import { TechIcons } from "../data/TechConfig"
 //? cursor and pointer events
 export default function TechIconsWrapper() {
+  const getBgGradientClass = (xp: number, index: number) => {
+    if (xp >= index) {
+      switch (index) {
+        case 1:
+          return "bg-gradient-1"
+        case 2:
+          return "bg-gradient-2"
+        case 3:
+          return "bg-gradient-3"
+        case 4:
+          return "bg-gradient-4"
+        default:
+          return "bg-gradient-default"
+      }
+    } else {
+      return "bg-gradient-default"
+    }
+  }
   return (
     <div className="flex flex-wrap items-center justify-center gap-5 px-[4%] py-10 md:px-[1%] md:py-16 lg:px-[4%] lg:py-20">
       {TechIcons &&
@@ -8,24 +26,42 @@ export default function TechIconsWrapper() {
           return (
             <div
               key={i}
-              className="h-42 w-42 group m-2 select-none p-3 hover:bg-dark-accent/50 hover:shadow-lg hover:scale-110 ease-in-out duration-200 "
+              className="h-42 w-42 group m-2 select-none p-3 duration-700 ease-in-out hover:scale-110 hover:bg-dark-accent/50 hover:shadow-lg "
             >
               <img
                 src={icon}
                 alt={name}
                 height={112}
                 width={112}
-                className="object-fit pointer-events-none h-28 w-28 drop-shadow-[0_10px_10px_rgba(255,255,255,0.08)]"
+                className="object-fit pointer-events-none h-28 w-28 drop-shadow-[0_8px_8px_rgba(255,255,255,0.08)] group-hover:drop-shadow-[0_16px_16px_rgba(255,255,255,0.15)] delay-100"
               />
               <p className="p-1 text-center text-lg">{name}</p>
               {/* Progress bar */}
-              <div className="h-5 rounded-sm bg-gray-200 group-hover:visible invisible">
-                <div
-                  className="hidden xp-bar group-hover:flex h-full items-center justify-center bg-blue-500"
-                  style={{ width: xp }}
-                >
-                  <span className="text-black font-semibold">{xp}</span>
-                </div>
+              <div className="invisible flex h-5 justify-center divide-x-2 divide-white rounded-sm bg-gray-200 p-0.5 group-hover:visible">
+                <span
+                  className={`h-full w-1/4 delay-100 ease-in ${getBgGradientClass(
+                    xp,
+                    1,
+                  )}`}
+                ></span>
+                <span
+                  className={`h-full w-1/4 delay-150 ease-in ${getBgGradientClass(
+                    xp,
+                    2,
+                  )}`}
+                ></span>
+                <span
+                  className={`h-full w-1/4 delay-200 ease-in ${getBgGradientClass(
+                    xp,
+                    3,
+                  )}`}
+                ></span>
+                <span
+                  className={`h-full w-1/4 delay-[250ms] ease-in ${getBgGradientClass(
+                    xp,
+                    4,
+                  )}`}
+                ></span>
               </div>
               {/* <p className="hidden group-hover:block">{xp}</p> */}
             </div>
