7ea68e694ca0aac879646e8b80acfdb74a602524
Kutimo <marius-oliver@hotmail.com>
2023-12-28 13:58:24 +0100

    Refactor TechIconsWrapper component to include Switch and Tooltip

--- src/components/TechIconsWrapper.tsx
+++ src/components/TechIconsWrapper.tsx
@@ -1,4 +1,6 @@
 import { TechIcons } from "$data/TechConfig"
+import Switch from "./common/Switch"
+import Tooltip from "./common/Tooltip"
 
 export default function TechIconsWrapper() {
   const getBgGradientClass = (xp: number, index: number) => {
@@ -20,55 +22,60 @@ export default function TechIconsWrapper() {
     }
   }
   return (
-    <div className="flex flex-wrap items-center justify-center gap-5 px-[4%] py-10 md:px-[1%] md:py-16 lg:px-[4%] lg:py-20">
-      {TechIcons &&
-        TechIcons.map(({ name, xp, icon }, i) => {
-          return (
-            <div
-              key={i}
-              className="h-42 w-42 group m-2 select-none p-3 duration-700 ease-in-out hover:scale-110 hover:bg-dark-accent/50 hover:drop-shadow-lg-black "
-            >
-              <img
-                src={icon}
-                alt={name}
-                height={112}
-                width={112}
-                className="object-fit pointer-events-none h-28 w-28 drop-shadow-lg-black delay-100"
-              />
-              <p className="p-1 text-center text-lg">{name}</p>
+    <div className="flex flex-col items-end px-[4%] py-10 md:px-[1%] md:py-16 lg:px-[4%] lg:py-20">
+      <Tooltip content="Show all">
+        <Switch />
+      </Tooltip>
+      <div className="flex flex-wrap items-center justify-center gap-5 ">
+        {TechIcons &&
+          TechIcons.map(({ name, xp, icon }, i) => {
+            return (
+              <div
+                key={i}
+                className="h-42 w-42 mt-5 group m-2 select-none p-3 duration-700 ease-in-out hover:scale-110 hover:bg-dark-accent/50 hover:drop-shadow-lg-black "
+              >
+                <img
+                  src={icon}
+                  alt={name}
+                  height={112}
+                  width={112}
+                  className="object-fit pointer-events-none h-28 w-28 drop-shadow-lg-black delay-100"
+                />
+                <p className="p-1 text-center text-lg font-serif font-semibold">{name}</p>
 
-              {/*
+                {/*
               // * Power bar
              */}
-              <div className="invisible flex h-5 justify-center divide-x-2 divide-white rounded-sm bg-gray-200 p-0.5 group-hover:visible">
-                <span
-                  className={`h-full w-1/4 delay-100 ease-in ${getBgGradientClass(
-                    xp,
-                    1,
-                  )}`}
-                ></span>
-                <span
-                  className={`h-full w-1/4 delay-150 ease-in ${getBgGradientClass(
-                    xp,
-                    2,
-                  )}`}
-                ></span>
-                <span
-                  className={`h-full w-1/4 delay-200 ease-in ${getBgGradientClass(
-                    xp,
-                    3,
-                  )}`}
-                ></span>
-                <span
-                  className={`h-full w-1/4 delay-[250ms] ease-in ${getBgGradientClass(
-                    xp,
-                    4,
-                  )}`}
-                ></span>
+                <div className="invisible flex h-5 justify-center divide-x-2 divide-white rounded-sm bg-gray-200 p-0.5 group-hover:visible">
+                  <span
+                    className={`h-full w-1/4 delay-100 ease-in ${getBgGradientClass(
+                      xp,
+                      1,
+                    )}`}
+                  ></span>
+                  <span
+                    className={`h-full w-1/4 delay-150 ease-in ${getBgGradientClass(
+                      xp,
+                      2,
+                    )}`}
+                  ></span>
+                  <span
+                    className={`h-full w-1/4 delay-200 ease-in ${getBgGradientClass(
+                      xp,
+                      3,
+                    )}`}
+                  ></span>
+                  <span
+                    className={`h-full w-1/4 delay-[250ms] ease-in ${getBgGradientClass(
+                      xp,
+                      4,
+                    )}`}
+                  ></span>
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+      </div>
     </div>
   )
 }
