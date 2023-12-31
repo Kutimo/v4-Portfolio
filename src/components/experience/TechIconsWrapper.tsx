d86fa7714e3f87bd45490b047f2ff088fe4e1c9f
Kutimo <marius-oliver@hotmail.com>
2023-12-31 17:14:54 +0100

    prettier and eslint

--- src/components/experience/TechIconsWrapper.tsx
+++ src/components/experience/TechIconsWrapper.tsx
@@ -40,8 +40,9 @@ export default function TechIconsWrapper() {
             return (
               <div
                 key={i}
-                className={`h-42 w-42 group m-2 mt-6 select-none p-3 duration-700 ease-in-out ${isActive ? `scale-110 bg-dark-accent/50 drop-shadow-lg-black` : ""
-                  }hover:scale-110 hover:bg-dark-accent/50 hover:drop-shadow-lg-black`}
+                className={`h-42 w-42 group m-2 mt-6 select-none p-3 duration-700 ease-in-out ${
+                  isActive ? `scale-110 bg-dark-accent/50 drop-shadow-lg-black` : ""
+                }hover:scale-110 hover:bg-dark-accent/50 hover:drop-shadow-lg-black`}
               >
                 <img
                   src={icon}
@@ -56,8 +57,9 @@ export default function TechIconsWrapper() {
               // * Power bar
              */}
                 <div
-                  className={`flex h-5 justify-center divide-x-2 divide-white rounded-sm bg-gray-200 p-0.5 ${isActive ? "visible" : "invisible"
-                    } group-hover:visible`}
+                  className={`flex h-5 justify-center divide-x-2 divide-white rounded-sm bg-gray-200 p-0.5 ${
+                    isActive ? "visible" : "invisible"
+                  } group-hover:visible`}
                 >
                   <span
                     className={`h-full w-1/4 delay-100 ease-in ${getBgGradientClass(
--- src/pages/home/sections/Experience.tsx
+++ src/pages/home/sections/Experience.tsx
@@ -4,11 +4,14 @@ import ExperienceList from "$components/experience/ExperienceList"
 export default function Experience() {
   return (
     // TODO: Fix font sizes (font is pixelated)
-    <section id="experience" className="mt-10 h-fit w-full text-white flex flex-col px-[4%] py-10 md:px-[1%] md:py-16 lg:px-[4%] lg:py-20">
+    <section
+      id="experience"
+      className="mt-10 flex h-fit w-full flex-col px-[4%] py-10 text-white md:px-[1%] md:py-16 lg:px-[4%] lg:py-20"
+    >
       <h2 className="mx-5 w-fit bg-dark-accent p-2 font-sans text-4xl font-bold text-light-shade-100 drop-shadow-lg-black ">
         Experience and skills
       </h2>
-      <div className="flex gap-3 mt-10">
+      <div className="mt-10 flex gap-3">
         <TechIconsWrapper />
         <ExperienceList />
       </div>
