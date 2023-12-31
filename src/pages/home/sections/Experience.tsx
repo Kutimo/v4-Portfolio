9ed78e8d2b76e8bed2f9ea022e0596405d4df3f2
Kutimo <marius-oliver@hotmail.com>
2023-12-31 15:11:33 +0100

    changed height

--- src/pages/home/sections/Experience.tsx
+++ src/pages/home/sections/Experience.tsx
@@ -3,12 +3,9 @@ import TechIconsWrapper from "$components/TechIconsWrapper"
 export default function Experience() {
   return (
     // TODO: Fix font sizes (font is pixelated)
-    <section id="experience" className="mt-20 h-96 w-full text-white">
+    <section id="experience" className="mt-5 w-full text-white h-fit">
       <h2 className="font-sans text-4xl font-bold p-2 bg-dark-accent w-fit text-light-shade-100 drop-shadow-lg-black mx-5 ">Experience and skills</h2>
       <TechIconsWrapper />
-      <div>
-        <h3>Tools</h3>
-      </div>
     </section>
   )
 }
