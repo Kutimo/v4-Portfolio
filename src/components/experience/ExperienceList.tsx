4fde52e08e2df5e0d7ba39c394590a11b4d065e8
Kutimo <marius-oliver@hotmail.com>
2023-12-31 16:14:28 +0100

    Fix component imports and update experience config

--- src/components/experience/ExperienceList.tsx
+++ src/components/experience/ExperienceList.tsx
@@ -1,13 +1,19 @@
+
 import Tag from "$components/common/Tag";
+
+
+
 
 export default function ExperienceList() {
   return (
     <article>
+      {/* {TechIcons &&
+        TechIcons.map(({ name, xp, icon }, i) => {
+          return ( */}
       <h3>header</h3>
       <p>date</p>
       <ul>
         <li>about text</li>
-        {/* <li>tags</li> */}
         <Tag text="tag" />
       </ul>
     </article>
---
+++ src/data/ExperienceConfig.ts
@@ -0,0 +1,16 @@
+export const ExperienceList: Array<{
+  title: string
+  place: string
+  date: string
+  description: string
+  tech: Array<string>
+}> = [
+  {
+    title: "Frontend Development",
+    place: "@Gokstadakademiet",
+    date: "2021 - Present",
+    description:
+      "I am currently studying as a frontend developer at Gokstadakademiet, where I am learning the basics of HTML, CSS and JavaScript. I am also learning how to use React and TypeScript.",
+    tech: ["HTML", "CSS", "JavaScript", "React", "TypeScript"],
+  },
+]
