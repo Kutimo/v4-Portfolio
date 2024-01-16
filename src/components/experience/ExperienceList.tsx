b7b53c8fd7761a3d0858dfd6a0350ad734800e61
Kutimo <marius-oliver@hotmail.com>
2024-01-16 16:13:59 +0100

    Refactor experience list and tech icons wrapper components

--- src/components/experience/ExperienceList.tsx
+++ src/components/experience/ExperienceList.tsx
@@ -7,16 +7,16 @@ export default function ExperienceList() {
       {ExperienceConfig &&
         ExperienceConfig.map(({ title, place, date, description, tech }, i) => {
           return (
-            <div key={i} className="flex w-fit flex-col gap-2 bg-main p-3">
+            <div key={i} className="flex flex-col bg-main p-3 gap-1">
               <div>
-                <h3>
-                  {title} <span className="text-light-shade-500">{place}</span>
+                <h3 className="text-2xl">
+                  {title} <span className="text-light-shade-400">{place}</span>
                 </h3>
-                <p className="font-sans text-sm italic">{date}</p>
+                <p className="font-sans text-sm italic mt-1">{date}</p>
               </div>
-              <ul className="flex flex-col items-center gap-3">
+              <ul className="flex flex-col items-center gap-2">
                 <li className="w-[50ch]">{description}</li>
-                <li className="flex gap-1">
+                <li className="flex gap-1.5">
                   {tech && tech.map((tag, j) => <Tag key={j} text={tag} />)}
                 </li>
               </ul>
--- src/pages/home/sections/Experience.tsx
+++ src/pages/home/sections/Experience.tsx
@@ -9,9 +9,9 @@ export default function Experience() {
       className="mt-10 flex h-fit w-full flex-col px-[4%] py-10 text-white md:px-[1%] md:py-16 lg:px-[4%] lg:py-20"
     >
       <h2 className="mx-5 w-fit bg-dark-accent p-2 font-sans text-4xl font-bold text-light-shade-100 drop-shadow-lg-black ">
-        Experience and skills
+        Skills and Experience
       </h2>
-      <div className="mt-10 flex gap-3">
+      <div className="mt-10 flex gap-3 border-2 p-3 rounded-md bg-dark-accent/20">
         <TechIconsWrapper />
         <ExperienceList />
       </div>
