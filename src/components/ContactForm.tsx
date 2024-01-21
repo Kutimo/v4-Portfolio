677c96ad980f9bfe943cdaa547a8a7ce459c1c40
Kutimo <marius-oliver@hotmail.com>
2024-01-21 22:27:47 +0100

    Refactor ContactForm component and update Contact section

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,31 +1,74 @@
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser'
+import { useRef } from "react"
+import emailjs from "@emailjs/browser"
+
+interface EmailJSResponse {
+  text: string
+}
+
+interface EmailJSError {
+  text: string
+}
 
 export const ContactForm = () => {
-  const form = useRef<HTMLFormElement>(null); // Initialize the form ref with null
+  const form = useRef<HTMLFormElement>(null)
 
-  const sendEmail = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
+  const sendEmail = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
 
     if (form.current) {
-      emailjs.sendForm("service_dvadyvr", "template_z9dfr1h", form.current, "6Lcwn_iudsCEftfMe")
-        .then((result: any) => {
-          console.log(result.text);
-        }, (error: any) => {
-          console.log(error.text);
-        });
+      emailjs
+        .sendForm(
+          "service_dvadyvr",
+          "template_z9dfr1h",
+          form.current,
+          "6Lcwn_iudsCEftfMe",
+        )
+        .then(
+          (result: EmailJSResponse) => {
+            console.log(result.text)
+            if (form.current) {
+              form.current.reset()
+            }
+          },
+          (error: EmailJSError) => {
+            console.log(error.text)
+          },
+        )
     }
-  };
+  }
 
   return (
-    <form ref={form} onSubmit={sendEmail} className='flex flex-col w-1/3 font-sans bg-light-shade-500 p-2 rounded-md gap-2'>
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className="mx-4 mt-10 flex flex-col gap-3 rounded-md border-2 border-white/50 bg-dark-accent/20 p-3 font-sans text-white drop-shadow-md md:mx-2 md:w-1/4"
+    >
       <label>Name</label>
-      <input type="text" name="user_name" className='rounded-md  bg-white p-1' />
+      <input
+        type="text"
+        name="user_name"
+        className="rounded-md bg-white/95 p-1 text-black"
+        required
+      />
       <label>Email</label>
-      <input type="email" name="user_email" className='rounded-md  bg-white p-1' />
+      <input
+        type="email"
+        name="user_email"
+        className="rounded-md bg-white/95 p-1 text-black"
+        required
+      />
       <label>Message</label>
-      <textarea name="message" className='rounded-md bg-white p-1' />
-      <button type="submit" >Send</button>
+      <textarea
+        name="message"
+        className="rounded-md bg-white/95 p-1 text-black"
+        required
+      />
+      <button
+        type="submit"
+        className="mt-2 bg-light-shade-500 p-3 text-white drop-shadow-md hover:cursor-pointer hover:bg-light-shade-300 hover:drop-shadow-lg-black "
+      >
+        Send
+      </button>
     </form>
-  );
-};
+  )
+}
--- src/pages/home/sections/Contact.tsx
+++ src/pages/home/sections/Contact.tsx
@@ -2,8 +2,10 @@ import { ContactForm } from "$components/ContactForm"
 
 export default function Contact() {
   return (
-    <section id="contact" className="mt-20 h-96">
-      <div>Contact</div>
+    <section id="contact" className="mt-10 ">
+      <h3 className="mx-5 w-fit bg-dark-accent p-2 font-sans text-3xl font-bold text-light-shade-100 drop-shadow-lg-black md:text-5xl">
+        Contact
+      </h3>
       <ContactForm />
     </section>
   )
