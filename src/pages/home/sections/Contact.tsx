c98d7cfef46ec07b76688e07672a0614943306fc
Kutimo <marius-oliver@hotmail.com>
2024-01-21 23:04:10 +0100

    added illustrations and moved padding from experience to home

--- src/pages/home/sections/Contact.tsx
+++ src/pages/home/sections/Contact.tsx
@@ -1,4 +1,5 @@
 import { ContactForm } from "$components/ContactForm"
+import contact_illustration from "$svg/illustrations/contact_illustration.svg"
 
 export default function Contact() {
   return (
@@ -6,7 +7,10 @@ export default function Contact() {
       <h3 className="mx-5 w-fit bg-dark-accent p-2 font-sans text-3xl font-bold text-light-shade-100 drop-shadow-lg-black md:text-5xl">
         Contact
       </h3>
-      <ContactForm />
+      <div className="flex items-center justify-evenly">
+        <ContactForm />
+        <img src={contact_illustration} alt="contact illustration" height={375} width={375} className="mt-40" />
+      </div>
     </section>
   )
 }
--- src/pages/home/sections/Experience.tsx
+++ src/pages/home/sections/Experience.tsx
@@ -6,7 +6,7 @@ export default function Experience() {
     // TODO: Fix font sizes (font is pixelated)
     <section
       id="experience"
-      className="mt-10 flex h-fit w-full flex-col px-[4%] py-10 text-white md:px-[1%] md:py-16 lg:px-[4%] lg:py-20"
+      className="mt-10 flex h-fit w-full flex-col text-white"
     >
       <h2 className="mx-5 w-fit bg-dark-accent p-2 font-sans text-3xl font-bold text-light-shade-100 drop-shadow-lg-black md:text-5xl ">
         Skills and Experience
