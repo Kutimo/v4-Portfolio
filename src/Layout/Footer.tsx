c4d0858f467613e0f8bbee00c816c6ff69022467
Kutimo <marius-oliver@hotmail.com>
2024-01-22 01:01:43 +0100

    Add social media icons to footer

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,3 +1,13 @@
+import { CiLinkedin } from 'react-icons/ci'
+import { FaGithub } from 'react-icons/fa'
+
 export default function Footer() {
-  return <div>footer</div>
+  return (
+    <footer className='p-5'>
+      <div className='flex'>
+        <CiLinkedin size={30} />
+        <FaGithub size={30} />
+      </div>
+    </footer>
+  )
 }
