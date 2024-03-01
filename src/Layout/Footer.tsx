d46caf3140798e3a55e649aefa8fbb7b557c7c0d
Kutimo2 <marius-oliver@hotmail.com>
2024-03-01 15:18:07 +0100

    add a tags and style to component

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -2,11 +2,22 @@ import { CiLinkedin } from 'react-icons/ci'
 import { FaGithub } from 'react-icons/fa'
 
 export default function Footer() {
+  const today = new Date()
+  const year = today.getFullYear()
+
   return (
-    <footer className='p-5'>
-      <div className='flex'>
-        <CiLinkedin size={30} />
-        <FaGithub size={30} />
+    <footer className='p-5 bg-black flex justify-evenly'>
+      <div className='flex flex-col font-serif'>
+        <span className='text-white text-center'>Designed and Developed by </span>
+        <span className='text-white text-center'>Marius Kristiansen @ {year}</span>
+      </div>
+      <div className='flex gap-10 '>
+        <a href="https://www.linkedin.com/in/marius-kristiansen-280b3b128/" target="_blank" className='text-white hover:text-light-shade-200 duration-300 '>
+          <CiLinkedin size={30} className='hover:scale-110' />
+        </a>
+        <a href="https://github.com/Kutimo" target="_blank" className='text-white hover:text-light-shade-200 duration-300 '>
+          <FaGithub size={30} className='hover:scale-110' />
+        </a>
       </div>
     </footer>
   )
